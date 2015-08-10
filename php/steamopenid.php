1709235d1c4253c36639e960f27bfc0cd2e65e43
who codes <cam@strafeodyssey.com>
2015-08-10 17:50:37 -0400

    Include logging and IP locking

--- php/steamopenid.php
+++ php/steamopenid.php
@@ -10,6 +10,9 @@
 			'api-key' 		=> '',
 			'session-salt' => '',
 			'token-prefix' => 'whocodestest-',
+			'max-sessions' 	=> 5,
+			'logging'		=> true,
+			'session-iplock' => false,
 		);
 		private $Database = array(
 			'host' 		=> '',
@@ -17,13 +20,28 @@
 			'pass'		=> '',
 			'db' 		=> '',
 		);
-		
+
+		/* http://php.net/manual/en/function.error-log.php */
+		private $LogType = 3;
+		private $LogLoc = __DIR__ . '/logs/steamopenid.log';
+
+		private function log($message){
+			if($this->Config['logging']){
+				if($LogType > 0){
+					error_log($message, $this->LogType, $this->LogLoc);
+				}else{
+					error_log($message);
+				}
+			}
+		}
+
 		private $Database_Handle;
 		private $Database_Connected = true;
 
 		private $OpenID;
 		private $LoggedIn = false;
 		private $Auth;
+
 
 		function __construct($login = false){
 			$this->OpenID = new LightOpenID($this->Config['openid-domain']);
@@ -35,18 +53,34 @@
 				$this->Database_Handle->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 				$this->Database_Handle->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
 			} catch (PDOException $exception){
+				$this->log('Error in construct @ initial connection: ' . $exception);
 				$this->Database_Connected = false;
 			}
 			unset($this->Database);
 
-			if($this->Database_Connected == false)
-				die('Error');
+			if($this->Database_Connected == false){
+				$this->log('Unable to connect to database');
+				return;
+			}
 
 			if(isset($_COOKIE[$this->Config['token-prefix'] . 'steam-token'])){
 				$old_token = $_COOKIE[$this->Config['token-prefix'] . 'steam-token'];
 				try{
-					$query = $this->Database_Handle->prepare("SELECT auth FROM sessions WHERE token = :oldtoken;");
+					$statement = "SELECT auth FROM sessions WHERE token = :oldtoken";
+
+					if($this->Config['session-iplock']){
+						$statement .= "AND address = :userip";
+					}
+
+					$statement .= ";";
+
+					$query = $this->Database_Handle->prepare();
 					$query->bindParam(':oldtoken', $old_token, PDO::PARAM_STR);
+
+					if($this->Config['session-iplock']){
+						$query->bindParam(':userip', $_SERVER['REMOTE_ADDR'], PDO::PARAM_STR);
+					}
+
 					$query->execute();
 
 					$result = $query->fetch(PDO::FETCH_ASSOC);
@@ -56,7 +90,7 @@
 						$this->Auth = $result['auth'];
 					}
 				} catch (PDOException $exception){
-
+					$this->log('Error at token verification: ' . $exception);
 				}
 			}
 
@@ -70,24 +104,55 @@
 					if($this->OpenID->validate()){
 						$temp_auth = str_replace('http://steamcommunity.com/openid/id/', '', $this->OpenID->identity);
 
-						try{
-							$query = $this->Database_Handle->prepare("DELETE FROM sessions WHERE auth = :sessionauth;");
-							$query->bindParam(':sessionauth', $temp_auth, PDO::PARAM_INT);
-							$query->execute();
-						} catch (PDOException $exception){
-							//die('Error 003');
+						if($this->Config['max-sessions'] <= 1){
+							try{
+								$query = $this->Database_Handle->prepare("DELETE FROM sessions WHERE auth = :sessionauth;");
+								$query->bindParam(':sessionauth', $temp_auth, PDO::PARAM_INT);
+								$query->execute();
+							} catch (PDOException $exception){
+								$this->log('Error deleting sessions (1): ' . $exception);
+							}
+						}else{
+							try{
+								$query = $this->Database_Handle->prepare("SELECT COUNT(*) FROM sessions WHERE auth = :sessionauth;");
+								$query->bindParam(':sessionauth', $temp_auth, PDO::PARAM_INT);
+								$query->execute();
+
+								$session_amt = $query->fetchColumn();
+
+								$del_amt = 0;
+								if($session_amt > $this->Config['max-sessions']){
+									$del_amt = ($session_amt - ($this->Config['max-sessions'] + 1));
+								}else if($session_amt == $this->Config['max-sessions']){
+									$del_amt = 1;
+								}
+
+								if($del_amt != 0){
+									try{
+										$query = $this->Database_Handle->prepare("DELETE FROM sessions WHERE auth = :sessionauth LIMIT :sessionlimit;");
+										$query->bindParam(':sessionauth', $temp_auth, PDO::PARAM_INT);
+										$query->bindParam(':sessionlimit', ($del_amt), PDO::PARAM_INT);
+										$query->execute();
+									} catch(PDOException $exception){
+										$this->log('Error deleting sessions (2): ' . $exception);
+									}
+								}
+							} catch (PDOException $exception){
+								$this->log('Error deleting sessions (3): ' . $exception);
+							}
 						}
 
 						$new_token = md5($this->Config['session-salt'] . '-' . $temp_auth . '-' . uniqid($this->Config['session-salt'], true));
 
 						try{
-							$query = $this->Database_Handle->prepare("INSERT INTO sessions (auth, token) VALUES (:sessionauth, :sessiontoken);");
+							$query = $this->Database_Handle->prepare("INSERT INTO sessions (auth, token, address) VALUES (:sessionauth, :sessiontoken, :userip);");
 							$query->bindParam(':sessionauth', $temp_auth, PDO::PARAM_INT);
 							$query->bindParam(':sessiontoken', $new_token, PDO::PARAM_STR);
+							$query->bindParam(':userip', $_SERVER['REMOTE_ADDR'], PDO::PARAM_STR);
 
 							$query->execute();
 						} catch (PDOException $exception){
-							die('Error 004');
+							$this->log('Error creating token: ' . $exception);
 						}
 
 						setcookie($this->Config['token-prefix'] . 'steam-token', $new_token, time() + $this->Config['cookie-expire']);
